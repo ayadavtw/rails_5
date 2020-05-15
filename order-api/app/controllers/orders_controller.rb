35d0ba9acbe46ff6efa98c8f0469feb0da2527ca
ayadav <ayadav@thoughtworks.com>
2020-05-16 00:47:05 +0530

    Price for order being set from Product via httparty call to product service

--- order-api/app/controllers/orders_controller.rb
+++ order-api/app/controllers/orders_controller.rb
@@ -2,10 +2,11 @@ class OrdersController < ApplicationController
   include Devise::Controllers::Helpers
   before_action :authenticate_user!
   def create
+    product =  HTTParty.get("http://localhost:30001/products/#{order_params[:product]}")
     @order= Order.new(order_params)
     @order.user = current_user
+    @order.price = ActiveSupport::JSON.decode(product.body)["price"]
     if @order.save
-      SignupMailer.signup_email(current_user).deliver
       render json: @order , serializer: OrdersSerializer
     else
       render json: {errors: @order.errors}
@@ -13,6 +14,6 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order).permit(:product, :price)
+    params.require(:order).permit(:product)
   end
 end
